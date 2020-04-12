f2acd96cea932c9c8c04d31524b8c1ee98b6408e
Laurynas Butkus <laurynas.butkus@gmail.com>
2020-04-13 02:13:53 +0300

    Fix float

--- prometheus.js
+++ prometheus.js
@@ -7,7 +7,7 @@ const garageGateGauge = new client.Gauge({ name: 'device_garage_gate', help: 'Ga
 const garageGateCounter = new client.Counter({ name: 'device_garage_gate_counter', help: 'Garage gate button clicks counter' });
 
 module.exports = (app) => {
-  app.on('devices:garage-temperature', value => garageTemperatureGauge.set(value));
+  app.on('devices:garage-temperature', value => garageTemperatureGauge.set(parseFloat(value)));
   app.on('devices:garage-gate', value => garageGateGauge.set(value));
   app.on('devices:garage-gate:click', () => garageGateCounter.inc());
 
