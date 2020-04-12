733384ebe07e116bbed35d57a8e18322aeea23c0
Laurynas Butkus <laurynas.butkus@gmail.com>
2020-04-13 00:35:17 +0300

    Use events for garage door

--- devices.js
+++ devices.js
@@ -1,7 +1,16 @@
 const GarageTemperatureSensor = require('./devices/temperature_sensor');
+const GarageGate = require('./devices/garage_gate');
 
 module.exports = (app) => {
   GarageTemperatureSensor(temperature => {
     app.emit('devices:garage-temperature', temperature);
   });
+
+  const garageGate = GarageGate({
+    pin: app.get('garage_gate_pin'),
+    buttonPin: app.get('garage_gate_button_pin'),
+    onChange: value => app.emit('devices:garage-gate', value),
+  });
+
+  app.on('devices:garage-gate:click', () => garageGate.click());
 }
--- sockets.js
+++ sockets.js
@@ -1,25 +1,16 @@
 module.exports = function(app, io) {
-  var state = {};
+  const state = {};
 
-  function emitState(socket) {
-    socket.emit('state', state);
-  };
+  const emitState = (socket) => socket.emit('state', state);
 
-  function updateState(key, value) {
+  const updateState = (key, value) => {
     state[key] = value;
     emitState(io);
     console.log(state);
   };
 
   app.on('devices:garage-temperature', temperature => updateState('garageTemperature', temperature));
-
-  var garageGate = require('./devices/garage_gate')({
-    pin: app.get('garage_gate_pin'),
-    buttonPin: app.get('garage_gate_button_pin'),
-    onChange: function(value) {
-      updateState('garageGate', value);
-    }
-  });
+  app.on('devices:garage-gate', value => updateState('garageGate', value));
 
   io.on('connection', function(socket) {
     console.log('socket connected');
@@ -31,7 +22,7 @@ module.exports = function(app, io) {
 
     socket.on('garage_gate_button_click', function() {
       console.log('garage gate button clicked');
-      garageGate.click();
+      app.emit('devices:garage-gate:click');
     });
   });
 };
