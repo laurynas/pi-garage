224a538a0d34381ce51da83120d7369c90b79168
Laurynas Butkus <laurynas.butkus@gmail.com>
2020-04-13 13:36:59 +0300

    Debug cpu temp

--- devices/cpu_temperature_sensor.js
+++ devices/cpu_temperature_sensor.js
@@ -1,7 +1,11 @@
 const si = require('systeminformation');
 
 const CPUTemperatureSensor = (callback) => {
-  const update = () => si.cpuTemperature(value => callback(value / 1000));
+  const update = () => si.cpuTemperature(value => {
+    console.log(value);
+    console.log(value / 1000);
+    callback(value / 1000)
+  });
 
   update();
   setInterval(() => update(), 60 * 1000);
