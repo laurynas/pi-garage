2184c3a38d5f82ec48534ee42c44936780f017a1
Laurynas Butkus <laurynas.butkus@gmail.com>
2020-04-13 13:57:46 +0300

    Fix click counter

--- prometheus.js
+++ prometheus.js
@@ -24,7 +24,7 @@ module.exports = (app) => {
   app.on('devices:cpu-temperature', value => thermometer.set({ location: 'cpu' }, value));
   app.on('devices:garage-temperature', value => thermometer.set({ location: 'garage' }, value));
   app.on('devices:garage-gate', value => opening.set({ location: 'garage_gate' }, value));
-  app.on('devices:garage-gate:click', () => clickCounter.labels('location', 'garage_gate').inc());
+  app.on('devices:garage-gate:click', () => clickCounter.inc({ location: 'garage_gate' }));
 
   app.get('/metrics', (req, res) => {
     res.set('Content-Type', client.register.contentType);
