c32f092d50cf2ccd8c0b5a67fe6c15195034f125
Laurynas Butkus <laurynas.butkus@gmail.com>
2020-04-13 13:53:34 +0300

    Use prometheus labels

--- prometheus.js
+++ prometheus.js
@@ -8,14 +8,23 @@ const thermometer = new client.Gauge({
   labelNames: ['location'],
 });
 
-const garageGateGauge = new client.Gauge({ name: 'device_garage_gate', help: 'Garage gate state' });
-const garageGateCounter = new client.Counter({ name: 'device_garage_gate_counter', help: 'Garage gate button clicks counter' });
+const opening = new client.Gauge({
+  name: 'pigarage_opening',
+  help: 'Door, window state (1 - closed, 0 - open)',
+  labelNames: ['location'],
+});
+
+const clickCounter = new client.Counter({
+  name: 'pigarage_click_counter',
+  help: 'Click counter',
+  labelNames: ['location'],
+});
 
 module.exports = (app) => {
   app.on('devices:cpu-temperature', value => thermometer.set({ location: 'cpu' }, value));
   app.on('devices:garage-temperature', value => thermometer.set({ location: 'garage' }, value));
-  app.on('devices:garage-gate', value => garageGateGauge.set(value));
-  app.on('devices:garage-gate:click', () => garageGateCounter.inc());
+  app.on('devices:garage-gate', value => opening.set({ location: 'garage_gate' }, value));
+  app.on('devices:garage-gate:click', () => clickCounter.labels('location', 'garage_gate').inc());
 
   app.get('/metrics', (req, res) => {
     res.set('Content-Type', client.register.contentType);
