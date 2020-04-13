99203c3ba72bfc4cfee3c4812088fea003e7a783
Laurynas Butkus <laurynas.butkus@gmail.com>
2020-04-13 15:54:12 +0300

    Cleanup

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -10,9 +8,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
