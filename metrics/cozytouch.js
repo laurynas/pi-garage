8adf28456049ad2eb676a2ac150034ac9fd0adb9
Laurynas Butkus <laurynas.butkus@gmail.com>
2020-04-13 16:51:13 +0300

    Build gauge

--- metrics/cozytouch.js
+++ metrics/cozytouch.js
@@ -1,3 +1,27 @@
+const lodash = require('lodash');
+
 module.exports = (client, app) => {
-  app.on('devices:cozytouch', state => console.log(state));
+  const registry = {}
+
+  const fetchGauge = (state) => {
+    if (!registry[state.name]) {
+      registry[state.name] = buildGauge(state);
+    }
+
+    return registry[state.name];
+  }
+
+  const buildGauge = (state) => {
+    const name = lodash.snakeCase(state.name);
+    console.log(state);
+    console.log(name);
+  }
+
+  app.on('devices:cozytouch', setup => {
+    setup.devices.forEach(device => {
+      device.states.forEach(state => {
+        const gauge = fetchGauge(state);
+      });
+    });
+  });
 }
