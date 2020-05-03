59cdc4095796faee5f798c215c6d7bace1708d9e
Laurynas Butkus <laurynas.butkus@gmail.com>
2020-05-03 12:31:46 +0000

    Add mqtt temperature

--- mqtt.js
+++ mqtt.js
@@ -6,12 +6,23 @@ const prefix = config.mqtt_discovery_prefix;
 const door = {
   id: 'gd1',
   name: 'Garage gate',
+  device_class: 'garage',
 };
 
-const baseTopic = prefix + '/cover/' + door.id;
-const configTopic = baseTopic + '/config';
-const commandTopic = baseTopic + '/set';
-const stateTopic = baseTopic + '/state';
+const t1 = {
+  id: 't1',
+  name: 'Garage',
+  device_class: 'temperature',
+};
+
+const gd1BaseTopic = prefix + '/cover/' + door.id;
+const gd1ConfigTopic = gd1BaseTopic + '/config';
+const gd1CommandTopic = gd1BaseTopic + '/set';
+const gd1StateTopic = gd1BaseTopic + '/state';
+
+const t1BaseTopic = prefix + '/sensor/' + t1.id;
+const t1ConfigTopic = t1BaseTopic + '/config';
+const t1StateTopic = t1BaseTopic + '/state';
 
 module.exports = (app) => {
   const client = mqtt.connect(`mqtt://${config.mqtt_broker}`, { 
@@ -22,13 +33,13 @@ module.exports = (app) => {
     console.log('mqtt connected');
   });
 
-  client.subscribe(commandTopic);
+  client.subscribe(gd1CommandTopic);
 
   client.on('message', (topic, message) => {
     console.log("Got mqtt message: %s %s", topic, message);
     
     switch(topic) {
-      case commandTopic:
+      case gd1CommandTopic:
         app.emit('devices:garage-gate:click');
         break;
     }
@@ -36,13 +47,26 @@ module.exports = (app) => {
 
   app.on('devices:garage-gate', value => {
     const state = value ? 'closed' : 'open';
-    client.publish(stateTopic, state, { retain: true });
+    client.publish(gd1StateTopic, state, { retain: true });
   });
 
-  client.publish(configTopic, JSON.stringify({
+  app.on('devices:garage-temperature', value => {
+    client.publish(t1StateTopic, value.toString(), { retain: true });
+  });
+
+  client.publish(gd1ConfigTopic, JSON.stringify({
+    device_class: door.device_class,
     name: door.name,
-    command_topic: commandTopic,
-    state_topic: stateTopic,
+    command_topic: gd1CommandTopic,
+    state_topic: gd1StateTopic,
+  }), {
+    retain: true,
+  });
+
+  client.publish(t1ConfigTopic, JSON.stringify({
+    device_class: t1.device_class,
+    name: t1.name,
+    state_topic: t1StateTopic,
   }), {
     retain: true,
   });
